4d16ae5183e3b4b47493634cfd577cb345932723
cmsmith91 <colleensmith91@gmail.com>
2021-11-02 16:19:59 -0400

    add r script and data

---
+++ inat_analysis/iNatAnalysis_blinded_final.R
@@ -0,0 +1,203 @@
+rm(list=ls())
+setwd("inat_analysis")
+library(tidyverse)
+library(caper) #for calculating d-statistic from Fritz and Purvis 2009
+library(furrr)
+library(phylolm)
+library(lme4)
+select=dplyr::select; map =purrr::map
+
+#upload data formatted for phyloglm analysis
+data=read.csv("df_inat_02nov2021.csv")
+rownames(data)=data$X
+data_df = data 
+
+#get rid of wind pollinated plants
+new_df = data_df[data_df$pollination=='biotic',] 
+new_df$abund_scaled=as.vector(scale(new_df$effort_corrected_abund))
+
+
+#upload and format phylogenetic tree of the data
+angio_tree=readRDS("tree_inat_angiosperms_30april2021.rds")
+scenario3 = angio_tree$scenario.3
+scenario3$node.label<-NULL
+new_tree=keep.tip(scenario3,gsub(" ","_",new_df$species_underscore))
+
+#simulate data for running the blinded analysis 
+sim_dat = data.frame(Y = array(0, dim = length(new_tree$tip.label)), X = new_df$abund_scaled, 
+                     row.names = new_tree$tip.label)
+beta0= -2.95  
+beta1 = 0.3
+beta_matrix=matrix(c(beta0,beta1),nrow=2)
+set.seed(457)
+a=binaryPGLMM.sim(Y~X, data = sim_dat, phy=new_tree,s2=.5, nrep = 1,
+                  B=beta_matrix)
+
+y_val=a$Y
+new_df$blind_y=y_val
+with(new_df,boxplot(abund_scaled~blind_y))
+table(new_df$category)
+
+#upload data for the paired analysis
+sisters=read_csv("sisters_02nov2021.csv") %>% 
+    filter(keep & !focal_a_crop) #get rid of plants that don't meet criteria
+
+set.seed(100) #set seed for blinding the data: randomizing the predictor variables within pairs 
+#change data into a long format (separate rows for each plant in a pair)
+sisters_long=sisters %>% mutate(pair_id=1:n()) %>% 
+    select(focal_genus,sister_genus,dist,pair_id) %>%
+    pivot_longer(!dist & !pair_id,names_to='type', values_to='plant_name') %>%
+    select(-type) %>% select(plant_name,pair_id,dist) %>% 
+    split(.$pair_id) %>% map_dfr(function(df){ #split by pair id, and randomize which plant is the host plant and which is a non-host
+        df %>% 
+            mutate(category_rand=sample(c('specialist_host','control'),2))})
+        
+#make a vector of all focal genera in the paired analysis, then separate by family pair and genera pairs
+focal_genera=c(sisters$focal_genus,sisters$sister_genus)
+
+#format the data for the paired analysis
+paired_genera=new_df %>% 
+    filter(genus %in% focal_genera & !family %in% focal_fams) %>% 
+    dplyr::select(genus,family,category,pollination,effort_corrected_abund,raw_abund) %>%
+    mutate(plant_name=genus,rank='genus')
+paired_df=paired_genera  %>% 
+    left_join(sisters_long) %>%mutate(pair_id=as.factor(pair_id))
+
+#run paired analysis
+#add a column for ln abundance
+paired_df=paired_df %>% mutate(log_abund=log(effort_corrected_abund)) #natural log abundance
+
+#singular fit: Probably because there are
+# 2 obs per level of random effect?
+paired_mod=lmer(effort_corrected_abund~category_rand + (1| pair_id),data=paired_df)
+
+plot(paired_mod)
+boxplot(paired_df$effort_corrected_abund) #there's are some outliers
+err=as.vector(residuals(paired_mod))
+boxplot(err~paired_df$category_rand)
+summary(paired_mod)
+
+#other options:
+#paired t-test
+
+#reformat the data to visualize if differences are normally distributed
+test_mapfun=paired_df %>% split(.$pair_id)
+df=test_mapfun[[1]]
+df_differences=paired_df %>% split(.$pair_id) %>% map_dfr(function(df) {
+    
+    the_diff=df[df$category_rand=='specialist_host',]$effort_corrected_abund-df[df$category_rand=='control',]$effort_corrected_abund
+    the_diff_log=log10(df[df$category_rand=='specialist_host',]$effort_corrected_abund)-log10(df[df$category_rand=='control',]$effort_corrected_abund)
+    
+    data.frame(pair_id=df$pair_id[1],diff=the_diff,diff_log_abund=the_diff_log)
+    
+    })
+hist(df_differences$diff)
+hist(df_differences$diff_log_abund) #looks better - though this may change upon unblinding data though
+library(ggpubr)
+ggqqplot(df_differences$diff_log_abund) #looks ok
+shapiro.test(df_differences$diff_log_abund) #not sig different from a normal distribution
+
+
+#reformat the data so they're in a wide foramt for the t-test
+pairs_wide=paired_df %>% mutate(abund_log10=log10(effort_corrected_abund)) %>%
+    select(category_rand,pair_id,abund_log10) %>% 
+    pivot_wider(names_from=category_rand,values_from=abund_log10)
+t.test(Pair(specialist_host, control) ~ 1, ,alternative='greater',data = pairs_wide)
+less_or_more=ifelse(mean(df_differences$diff)<0,'less','more')
+print(paste0('Plants hosting specialist bees were, on average, ', abs(round(mean(df_differences$diff),3)*100), '% ',less_or_more,' abundant than close relatives not hosting specialist bees (median percent difference = ',round(median(df_differences$diff),3)*100,"%)"))
+
+# if the data are not normally distributed after unblinding, we will use a wilcoxin signed rank test
+wilcox.test(Pair(specialist_host, control) ~ 1, ,alternative='greater',data = pairs_wide)
+
+#make the figure
+#option 1) a boxplot or histogram 
+boxplot(df_differences$diff)
+hist(df_differences$diff)
+paired_df_blinded=paired_df %>% mutate(abund_log10=log10(effort_corrected_abund))
+
+#make transparent color for figure
+transparent_col=rgb(225,225,225,max=255,alpha=0)
+col2rgb(4)
+percent_col=40
+my_alpha=( percent_col) * 255 / 100
+blue_transparent=rgb(34,151,230,max=255,alpha=my_alpha)
+loc_nonhost=1.2; loc_host=1.8
+
+# pdf('paired_analysis_boxplot_02nov2021.pdf')
+par(mar=c(4.5,5.5,3,2),cex.lab=1.5,cex.axis=1.4,cex=1.5)
+with(paired_df_blinded,
+     stripchart(abund_log10~category_rand,
+                ylab='log10(effort-corrected abundance)',
+                xlab='Plant type',
+                group.names=c('non-host','host'),
+                vertical=T,pch=16,col=4,cex=.8,at=c(loc_nonhost,loc_host)))
+for(i in 1:nrow(pairs_wide)){segments(loc_nonhost, pairs_wide$control[i], loc_host, pairs_wide$specialist_host[i],lty=2,col=blue_transparent)}
+with(paired_df_blinded,boxplot(abund_log10~category_rand,boxwex=c(.35,.35),
+                               xaxt = "n" ,xlab='Plant type',pch=1,col=transparent_col,alpha=.1,at=c(loc_nonhost,loc_host),add=T))
+# dev.off()
+
+
+
+#run all angiosperm analysis on blinded data
+obs_mod=phyloglm(blind_y~abund_scaled,phy=new_tree,data=new_df,btol=40,method = c("logistic_MPLE"))
+summary(obs_mod)
+obs_beta=coef(obs_mod)[2]
+
+# calculate the power of the model and analysis method - see 'calculate power' r script
+
+# run permutations to assess the significance of model coefficients
+n_perm=999
+plan(multisession,workers=6)
+permut_output=1:n_perm %>% future_map(possibly(function(i){
+    new_y=sample(y_val)
+    mod=phyloglm(new_y~abund_scaled,phy=new_tree,data=new_df,btol=30,method = c("logistic_MPLE"))
+    null_beta=coef(mod)[2]
+    converged=mod$convergence==0
+    # 
+    tibble(null_beta=null_beta,converged=converged)
+},otherwise=NULL),.options=furrr_options(seed=T)) %>% bind_rows
+
+# check whether number of permutations =n_perm
+# if not, do more permtuations in units of 10
+# until you get the correct number of converged permutations
+while(sum(permut_output$converged)<n_perm){
+    add_me=1:10 %>% future_map(possibly(function(i){
+        new_y=sample(y_val)
+        mod=phyloglm(new_y~abund_scaled,phy=new_tree,data=new_df,btol=30,method = c("logistic_MPLE"))
+        null_beta=coef(mod)[2]
+        converged=mod$convergence==0
+        # 
+        tibble(null_beta=null_beta,converged=converged)
+    },otherwise=NULL),.options=furrr_options(seed=T)) %>% bind_rows
+    permut_output=permut_output %>% bind_rows(add_me)
+}
+
+#calculate pval from permutations
+null_betas=as.vector(permut_output$null_beta[1:n_perm])
+r=sum(null_betas>obs_beta)
+pval=(r+1)/(n_perm+1)
+
+#now plot the model prediction and results
+# get probabilities from the model coefficients
+obs_intercept=coef(obs_mod)[1]
+prob_host=exp(obs_intercept+obs_beta*new_df$abund_scaled)/(1+exp(obs_intercept+obs_beta*new_df$abund_scaled))
+prob_host=prob_host[order(prob_host)]
+abund_ordered=new_df$abund_scaled[order(new_df$abund_scaled)]
+
+#get effect sizes
+percent_increase=round((max(prob_host)-min(prob_host))/min(prob_host)*100,1)
+percent_decrease=round((max(prob_host)-min(prob_host))/max(prob_host)*100,1)
+
+if(obs_beta>0){
+    print(paste0('The least abundant angiosperm in our data had an ',round(min(prob_host)*100),'% chance of hosting pollen specialist bee species, whereas the most abundant angiosperm in our data had an ', round(max(prob_host)*100),'% chance.'))
+}
+if(obs_beta<0){
+    print(paste0('The least abundant angiosperm in our data had an ',round(max(prob_host)*100),'% chance of hosting pollen specialist bee species, whereas the most abundant angiosperm in our data had an ', round(min(prob_host)*100),'% chance.'))
+}
+
+# pdf('phyloglm_results_blinded.pdf',width=8)
+par(cex.lab=1.8,cex.axis=1.5,mar=c(4.8,5.1,3.1,2.1))
+plot(abund_ordered,prob_host,type='n',ylim=c(0,1),ylab='Probability of hosting a specialist bee',xlab='Abundance (corrected for effort and scaled)')
+lines(abund_ordered,prob_host,type = "l",lwd=3)
+points(new_df$abund_scaled,y_val,pch="|")
+# dev.off()
