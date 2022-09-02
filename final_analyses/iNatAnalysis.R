4a1cd8be3aac8b560b9ef829f451411fab47b9b8
cmsmith91 <colleensmith91@gmail.com>
2022-09-02 15:25:58 -0700

    cleaning

--- final_analyses/iNatAnalysis.R
+++ final_analyses/iNatAnalysis.R
@@ -2,7 +2,7 @@ rm(list=ls())
 library(tidyverse)
 library(furrr)
 library(phylolm)
-setwd("/Users/colleen/Dropbox/github/specialist_bees")
+setwd("specialist_bees")
 select=dplyr::select; map =purrr::map
 
 #upload data formatted for phyloglm analysis
@@ -13,6 +13,11 @@ data_df = data
 #get rid of wind pollinated plants
 new_df = data_df[data_df$pollination=='biotic',] 
 new_df$abund_scaled=as.vector(scale(new_df$effort_corrected_abund))
+
+#how many specialist bees?
+spec_bees = read_csv("final_analyses/bee_hosts_east.csv") %>%
+  filter(host_plant %in% new_df$genus)
+n_distinct(spec_bees$bee)
 
 #upload and format phylogenetic tree of the data
 angio_tree=readRDS("inat_analysis/tree_inat_angiosperms_30april2021.rds")
@@ -25,7 +30,6 @@ new_tree=keep.tip(scenario3,gsub(" ","_",new_df$species_underscore))
 sisters=read_csv("inat_analysis/sisters_02nov2021.csv") %>% 
     filter(keep & !focal_a_crop) #get rid of plants that don't meet criteria
 
-set.seed(100) #set seed for blinding the data: randomizing the predictor variables within pairs 
 
 #change data into a long format (separate rows for each plant in a pair)
 sisters_long=sisters %>% mutate(pair_id=1:n()) %>% 
@@ -33,7 +37,7 @@ sisters_long=sisters %>% mutate(pair_id=1:n()) %>%
     pivot_longer(!dist & !pair_id,names_to='type', values_to='plant_name') %>%
     select(-type) %>% select(plant_name,pair_id,dist) 
 
-#make a vector of all focal genera in the paired analysis, then separate by family pair and genera pairs
+#make a vector of all focal genera in the paired analysis
 focal_genera=c(sisters$focal_genus,sisters$sister_genus)
 
 #format the data for the paired analysis
@@ -42,12 +46,12 @@ paired_genera=new_df %>%
     dplyr::select(genus,family,category,pollination,effort_corrected_abund,raw_abund) %>%
     mutate(plant_name=genus,rank='genus')
 paired_df=paired_genera  %>% 
-    left_join(sisters_long) %>%mutate(pair_id=as.factor(pair_id))
+    left_join(sisters_long) %>%
+  mutate(pair_id=as.factor(pair_id))
 
 #run paired analysis
 #add a column for ln abundance
 paired_df=paired_df %>% mutate(abund_log10=log10(effort_corrected_abund))
-#run paired t-test
 
 #reformat the data to visualize if differences are normally distributed
 test_mapfun=paired_df %>% split(.$pair_id)
@@ -64,12 +68,6 @@ df_differences=paired_df %>% split(.$pair_id) %>% map_dfr(function(df) {
     })
 hist(df_differences$percent_diff)
 median(df_differences$percent_diff)
-paired_df %>% split(.$pair_id) %>% map_dfr(function(df) {
-    
-    
-    data.frame(specialist_host=df[df$category=='specialist_host',]$genus,nonhost=df[df$category=='nonhost',]$genus)
-    
-})
 
 # pdf('figures/histograms_differences.pdf',width=11)
 par(mfrow=c(1,2))
@@ -81,7 +79,7 @@ library(ggpubr)
 ggqqplot(df_differences$diff)
 shapiro.test(df_differences$diff)
 ggqqplot(df_differences$diff_log_abund) #looks ok
-shapiro.test(df_differences$diff_log_abund) #not sig different from a normal distribution
+shapiro.test(df_differences$diff_log_abund) #not significantly different from a normal distribution
 hist(df_differences$diff_log_abund)
 
 #reformat the data so they're in a wide foramt for the t-test
@@ -95,16 +93,14 @@ less_or_more=ifelse(mean(df_differences$diff)<0,'less','more')
 print(paste0('Plants hosting specialist bees were, on average, ', 
              abs(round(mean(df_differences$diff),3)*100), '% ',
              less_or_more,' abundant than close relatives not hosting specialist bees (median percent difference = ',round(median(df_differences$diff),3)*100,"%)"))
-df_differences
+
 
 #need to report diffs on a raw scale
-host_abund=mean(paired_df[paired_df$category=='specialist_host',]$effort_corrected_abund)
-nhost_abund=mean(paired_df[paired_df$category=='nonhost',]$effort_corrected_abund)
+host_abund = mean(paired_df[paired_df$category=='specialist_host',]$effort_corrected_abund)
+nhost_abund = mean(paired_df[paired_df$category=='nonhost',]$effort_corrected_abund)
 
 (host_abund-nhost_abund)/(nhost_abund)*100 #percent increase
-(host_abund)/(nhost_abund)#times abundant
-
-
+(host_abund)/(nhost_abund) #times abundant
 
 #what is the percent difference for the pair with the median difference?
 diff=pairs_wide$specialist_host-pairs_wide$nonhost
@@ -123,13 +119,13 @@ nonhost_abund2=middle_pair[middle_pair$category=='nonhost',]$effort_corrected_ab
 my_cols=RColorBrewer::brewer.pal(8,"Accent")
 color_vec=adjustcolor(with(paired_df,my_cols[as.factor(category)]),.2)
 color_vec2=adjustcolor(with(paired_df,my_cols[as.factor(category)]),.7)
-
+color_vec3=with(paired_df,my_cols[as.factor(category)])
 black_shade=adjustcolor('black',.4)
 loc_nonhost=1.2; loc_host=1.8
 
-# pdf('figures/paired_analysis_boxplot_19april2022.pdf')
+# pdf('figures/paired_analysis_boxplot_11june2022.pdf')
 
-par(mfrow=c(1,1),mar=c(4.5,5.5,3,2),cex.lab=1.5,cex.axis=1.4,cex=1.5)
+par(mfrow=c(1,1),mar=c(4.5,5.5,3,2),cex.lab=1.3,cex.axis=1,cex=1.5)
 with(paired_df,
      stripchart(abund_log10~(category),
                 ylab=expression('log'[10]*'(effort-corrected abundance)'),
@@ -141,23 +137,6 @@ with(paired_df,boxplot(abund_log10~category,boxwex=c(.35,.35),
                        xaxt = "n" ,xlab='Plant type',pch=1,col=unique(color_vec),alpha=.1,at=c(loc_nonhost,loc_host),add=T))
 # dev.off()
  
-# pdf('figures/paired_analysis_boxplot_raw.pdf')
-par(mar=c(4.5,5.5,3,2),cex.lab=1.5,cex.axis=1.4,cex=1.5)
-with(paired_df,
-     stripchart(effort_corrected_abund~category,
-                ylab='log10(effort-corrected abundance)',
-                xlab='Plant type',
-                group.names=c('non-host','host'),
-                vertical=T,pch=16,col=4,cex=.8,at=c(loc_nonhost,loc_host)))
-for(i in 1:nrow(pairs_wide)){segments(loc_nonhost, 10^pairs_wide$nonhost[i], loc_host, 10^pairs_wide$specialist_host[i],lty=2,col=blue_transparent)}
-with(paired_df,boxplot(effort_corrected_abund~category,boxwex=c(.35,.35),
-                       xaxt = "n" ,xlab='Plant type',pch=1,col=transparent_col,alpha=.1,at=c(loc_nonhost,loc_host),add=T))
- # dev.off()
-
- paired_df %>% group_by(category) %>% summarize(med= median(effort_corrected_abund))
- 
- paired_df %>% group_by(category) %>% summarize(med= median(abund_scaled))
-data.frame(pairs_wide)
 
 #summary of new_df
 paste0(sum(new_df$raw_abund),' observations')
@@ -201,7 +180,8 @@ null_betas=as.vector(permut_output$null_beta[1:n_perm])
 r=sum(null_betas>obs_beta)
 (pval=(r+1)/(n_perm+1))
 
-#bootstrap confidence intervals for the slope
+#we want bootstrap confidence intervals for the slope
+# to put on our figure
 #resample with replacement from the data 10000 times
 #need to incorporate residual uncertainity: eg https://www.youtube.com/watch?v=c3gD_PwsCGM
 
@@ -264,14 +244,6 @@ if(obs_beta<0){
     print(paste0('The least abundant angiosperm in our data had an ',round(max(prob_host)*100),'% chance of hosting pollen specialist bee species, whereas the most abundant angiosperm in our data had an ', round(min(prob_host)*100),'% chance.'))
 }
 
-#basic stats about sample size
-sum(new_df$raw_abund)
-nrow(paired_df)
-hosts=read_csv("~/Dropbox/Fall_2014/postdoc/inat project/data_current/bee_hosts_allUS.csv") %>% 
-    filter(plant_rank=="genus" & region=='east')
-data.frame(hosts %>% filter(host_plant %in% paired_df$genus) %>% group_by(host_plant) %>% 
-    summarize(n_distinct(bee)))
-paired_df%>% filter(category=="specialist_host" & !genus %in% hosts$host_plant)
 
 
 #add predictions to graph as well
@@ -282,11 +254,9 @@ new_ci_df=data.frame(t(boot_pred_cis)) %>% rename(lower_ci='X2.5.' ,upper_ci='X9
 
 
 # dev.off()
-#which are the 10 most common plants in the data
-head(new_df)
-new_df_arranged=new_df %>% arrange(desc(abund_scaled))
-new_df_arranged[1:20,]
 
+
+#format data to make histograms
 break_n=40
 abund_min=min(new_df$abund_scaled); abund_max=max(new_df$abund_scaled)
 my_breaks=seq(abund_min,abund_max,by=(abund_max-abund_min)/break_n) 
